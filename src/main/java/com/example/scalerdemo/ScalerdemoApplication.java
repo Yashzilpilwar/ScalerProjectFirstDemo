563dc80e2232e184db3f2507fe802842fc256cfc
Demo <jayzilpilwar@gmail.com>
2024-05-07 19:34:34 +0530

    MyFirstCommit

---
+++ src/main/java/com/example/scalerdemo/ScalerdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.scalerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalerdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScalerdemoApplication.class, args);
+    }
+
+}
